5783be817b553da8184926ccaf67f56f63edaad6
Douglas Brandão <douglasfrb@gmail.com>
2020-07-07 16:50:11 -0400

    Adding a logo in our application

--- src/components/Navigation/Toolbar/index.js
+++ src/components/Navigation/Toolbar/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import classes from './styles.css';
+import Logo from '../../Logo';
 
 const toolbar = props => (
   <header className={classes.Toolbar}>
     <div>MENU</div>
-    <div>LOGO</div>
+    <Logo />
     <nav>
       ...
     </nav>
