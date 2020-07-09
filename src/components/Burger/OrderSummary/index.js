1a7ffc34253591b7ec7a65676234d3ae9e3bfd13
Douglas Brandão <douglasfrb@gmail.com>
2020-07-09 01:32:42 -0400

    Improving performance with ShouldComponentUpdate

--- src/components/Burger/OrderSummary/index.js
+++ src/components/Burger/OrderSummary/index.js
@@ -1,38 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Aux from '../../../hoc/Aux';
 import Button from '../../UI/Button';
 
-const orderSummary = props => {
-  const ingredientsSummary = Object.keys(props.ingredients)
-    .map(igKey => {
-      return (
-        <li key={igKey}>
-          <span style={{ textTransform: 'capitalize ' }}>{igKey}</span>: {props.ingredients[igKey]}
-        </li>
-      )
-    });
+class OrderSummary extends Component {
+  // This could be a functional component, doesn't have to be a class
+  componentDidUpdate() {
+    console.log('Order Summary Update');
+  }
+  render() {
+    const ingredientsSummary = Object.keys(this.props.ingredients)
+      .map(igKey => {
+        return (
+          <li key={igKey}>
+            <span style={{ textTransform: 'capitalize ' }}>{igKey}</span>: {this.props.ingredients[igKey]}
+          </li>
+        )
+      });
 
-  return (
-    <Aux>
-      <h3>Your Order</h3>
-      <p>A Delicious order with the following ingredients:</p>
-      <ul>
-        {ingredientsSummary}
-      </ul>
-      <p><strong>Total price: {props.price}</strong></p>
-      <p>Continue to checkout</p>
-      <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
-      <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
-    </Aux>
-  )
+    return (
+      <Aux>
+        <h3>Your Order</h3>
+        <p>A Delicious order with the following ingredients:</p>
+        <ul>
+          {ingredientsSummary}
+        </ul>
+        <p><strong>Total price: {this.props.price}</strong></p>
+        <p>Continue to checkout</p>
+        <Button btnType="Danger" clicked={this.props.purchaseCancelled}>CANCEL</Button>
+        <Button btnType="Success" clicked={this.props.purchaseContinued}>CONTINUE</Button>
+      </Aux >);
+  }
 }
 
-orderSummary.propTypes = {
+OrderSummary.propTypes = {
   ingredients: PropTypes.object.isRequired,
   price: PropTypes.string.isRequired,
   purchaseCancelled: PropTypes.func.isRequired,
   purchaseContinued: PropTypes.func.isRequired
 }
 
-export default orderSummary;
+export default OrderSummary;
--- src/components/UI/Modal/index.js
+++ src/components/UI/Modal/index.js
@@ -1,25 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './styles.css';
 import Aux from '../../../hoc/Aux';
 import PropTypes from 'prop-types';
 import Backdrop from '../Backdrop';
 
-const modal = props => (
-  <Aux>
-    <Backdrop show={props.show} clicked={props.modalClosed} />
-    <div className={classes.Modal}
-      style={{
-        transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-        opacity: props.show ? '1' : '0'
-      }}>
-      {props.children}
-    </div>
-  </Aux>
-);
+class Modal extends Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.show !== this.props.show;
+  }
 
-modal.propTypes = {
+  componentDidUpdate() {
+    console.log('Modal will update');
+  }
+
+  render() {
+    return (
+      <Aux>
+        <Backdrop show={this.props.show} clicked={this.props.modalClosed} />
+        <div className={classes.Modal}
+          style={{
+            transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+            opacity: this.props.show ? '1' : '0'
+          }}>
+          {this.props.children}
+        </div>
+      </Aux>
+    );
+  }
+}
+
+Modal.propTypes = {
   show: PropTypes.bool.isRequired,
   modalClosed: PropTypes.func.isRequired
 }
 
-export default modal;
+export default Modal;
