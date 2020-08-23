49123cca21b6601f010384623d7c68d8f6641b40
Douglas Brandao <douglasfrb@gmail.com>
2020-08-23 15:24:37 -0400

    Outputting the orders

--- src/components/Order/index.js
+++ src/components/Order/index.js
@@ -1,13 +1,41 @@
 import React from "react";
 import classes from "./styles.css";
 
-const order = (props) => (
-  <div className={classes.Order}>
-    <p>Ingredients: Salad (1)</p>
-    <p>
-      Price: <strong>USD 5.43</strong>
-    </p>
-  </div>
-);
+const order = (props) => {
+  const ingredients = [];
+
+  for (let name in props.ingredients) {
+    ingredients.push({
+      name: name,
+      amount: props.ingredients[name],
+    });
+  }
+
+  const ingredientOutput = ingredients.map((ingredient) => {
+    return (
+      <span
+        style={{
+          textTransform: "capitalize",
+          display: "inline-block",
+          margin: "0 8px",
+          border: "1px solid gray",
+          padding: "5px",
+        }}
+        key={ingredient.name}
+      >
+        {ingredient.name} ({ingredient.amount})
+      </span>
+    );
+  });
+
+  return (
+    <div className={classes.Order}>
+      <p>Ingredients: {ingredientOutput}</p>
+      <p>
+        Price: <strong>USD {props.price.toFixed(2)}</strong>
+      </p>
+    </div>
+  );
+};
 
 export default order;
